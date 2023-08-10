f1a3175879b869ebf86ebad746cf022cc846d66e
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-10 19:44:18 +0300

    created Part 9

---
+++ PART-9/app.js
@@ -0,0 +1,32 @@
+const todoInput= document.getElementById("todo")
+//<input class="form-control" type="text" name="todo" id = "todo" placeholder="Bir Todo Girin">
+let element;
+
+
+element= todoInput;
+element = todoInput.classList
+
+// todoInput.classList.add("newClass");
+// todoInput.classList.add("newClass2");
+// todoInput.classList.remove("form-control");
+
+element= todoInput;
+element=todo.placeholder
+element =todoInput.getAttribute("placeholder")
+todoInput.setAttribute("placeholder","slmcnm")
+todoInput.setAttribute("title","input")
+
+
+element=todoInput
+// element = todoInput.hasAttribute("placeholder")
+todoInput.removeAttribute("name")
+
+
+
+console.log(element);
+
+
+
+
+
+
