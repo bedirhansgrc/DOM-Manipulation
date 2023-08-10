fd41d2a7c9f7371780a959d4fea740601eba44e1
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-10 15:07:52 +0300

    Created PART-1

---
+++ PART-1/app.js
@@ -0,0 +1,49 @@
+
+let value;
+
+value = document
+value = document.all
+value = document.all.length
+value = document.all[0];
+value = document.all[document.all.length-1];
+
+const elements = document.all;
+// console.log(elements);
+
+
+// for(let i = 0; i<elements.length; i++){
+//     console.log(elements[i]);
+// }
+
+// elements.forEach(function(element){
+//     console.log(element);
+// });
+
+// const collections =Array.from(document.all);
+
+// console.log(typeof collections);
+
+// collections.forEach(function(collection){
+//     console.log(collection);
+// })
+
+value = document.all[13]
+value = document.body
+value = document.head
+value = document.location
+value = document.location.hostname
+value = document.location.port
+
+value = document.URL
+
+value = document.characterSet
+
+
+
+console.log(value)
+
+
+
+
+
+
