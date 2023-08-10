9b3c4c62e5658a84d1685b5c1957ff5044bcf1cc
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-08-10 19:20:15 +0300

    created Part 7

---
+++ PART-7/app.js
@@ -0,0 +1,23 @@
+//Dinamik Element Silme
+
+
+const todoList = document.querySelector("ul.list-group")
+const todos = document.querySelectorAll("li.list-group-item");
+
+
+// Remove Metodu
+
+
+// todos[1].remove()
+
+
+//remove child
+
+
+// todoList.removeChild(todoList.lastElementChild)
+todoList.removeChild(todos[3])
+
+
+
+console.log(todo)
+console.log(todos)
